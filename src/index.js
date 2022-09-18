fb155b697a835fa87a8d82896d30a642465424a3
Ruslan Gusseinov <r.gusseinov@mail.ru>
2022-09-18 20:48:13 +0600

    setup: setup and configure eslint

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const r = true;
+if (r) {
+  const a = 10;
+  console.log("a", a);
+}
+
+for (let a = 1; a <= 10; a += 1) {
+  console.log(a);
+}
+
+function abc() {
+  console.log("sdfsdfg");
+}
+abc();
